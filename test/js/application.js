51e7cc0cecc19263368cb9fc9694bbafea770f9e
Thomas Rößler <twroessler@gmail.com>
2018-10-05 20:59:58 +0200

    added test cases

---
+++ test/js/application.js
@@ -0,0 +1,46 @@
+//var remote_host = "https://192.168.178.51:8080";
+var remote_host = "http://192.168.178.50:8080/";
+var remote_api_path = "api/"
+
+$(function(){
+    initHandler();
+});
+
+ /********************************
+  * Event handler
+  ********************************/
+
+function initHandler() {
+    $("#test-getprojects").click(function (e) { 
+        test_getProjects();
+    });
+
+    $("#test-searchByTitle").click(function (e) { 
+        test_searchByTitle("hallo");
+    });
+}
+
+ /********************************
+  * API Test Cases
+  ********************************/
+
+function test_getProjects() {
+    $.ajax({
+        url: remote_host + remote_api_path + "project",
+        success: function(result) {
+            $("#test-results").html(JSON.stringify(result));
+        }
+    });
+}
+
+function test_searchByTitle(search_term) {
+    $.ajax({
+        url: remote_host + remote_api_path + "project/search",
+        data: {
+            title: search_term
+        },
+        success: function(result) {
+            $("#test-results").html(JSON.stringify(result));
+        }
+    });
+}
