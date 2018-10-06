e20842ea4952dd5e349e03afb2f393563a58d615
Marcel Menk <mmenk@blaiq.biz>
2018-10-06 10:32:35 +0200

    [#] Change

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -1,3 +1,9 @@
 $('#filter-expand').on('click', function() {
   $('#filter-selection').toggle();
 });
+
+$('#donateBtn').on('click', function(event) {
+  event.preventDefault();
+  $('#donate').modal('hide');
+  $('#checkout').modal('show');
+});
